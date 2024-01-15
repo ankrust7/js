9afd0203a1087b3f62f1bcbb02da6ca2fbe4fb79
ankrust7 <149107247+ankrust7@users.noreply.github.com>
2024-01-15 17:09:52 +0000

    conversion of data type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+
+console.log("Aman");
+
+let name = "Aman"
+let age = 19
+let isLoggedIn = false
+
+// null is in itself is a standalone value
+// symbol => for uniqueness
+// object
+
+console.log(typeof null);
+// null will be defined as object
---
+++ 01_basics/03_conversionoperatio.js
@@ -0,0 +1,26 @@
+let score = "Aman"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber);
+ 
+// console.log(valueInNumber);
+
+// "33" -- 33
+// "33abc" -- NaN
+// "true" -- 1
+
+let isLoggedIn = 1
+let booleanIsLoggenIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggenIn);
+// 1 -- true , 0 -- false
+// "" -- false
+// "aman" -- true
+ let some = 33
+
+ let strin = String(some)
+ console.log(strin);
+ 
+ console.log(typeof strin);
